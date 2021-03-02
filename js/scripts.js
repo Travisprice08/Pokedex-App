dfd5751a01939275ed4025bb6416862cf12d4558
Travisprice08 <75191962+Travisprice08@users.noreply.github.com>
2021-03-01 20:34:03 -0800

    Variable
    -Cleared existing code
    -Created variable
    -Added array within variable
    -Added three objects to the array

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
-alert('Hello World')
-var favoriteFood = 'Jjajangmyeon';
-document.write( favoriteFood);
+let pokemonList = [
+  {
+    name: 'Bulbasaur',
+    height: '2\'04\"'
+    type: ['Grass','Poison']
+  },
+  {
+    name: 'Charmander',
+    height: '2\''
+    type: 'Fire'
+  },
+  {
+    name: 'Squirtle',
+    height: '1\'08\"'
+    type: 'Water'
+  },
+];
