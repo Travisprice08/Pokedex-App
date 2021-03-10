b18bd9c420c8414df6df42cb66abc65d3acefafa
Travisprice08 <75191962+Travisprice08@users.noreply.github.com>
2021-03-09 19:49:25 -0800

    Created For Loop
    Created for loop to list out pokemon and stats.
    
    Made changes to the individual heights for simlification.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,27 @@
 let pokemonList = [
   {
     name: 'Bulbasaur',
-    height: '2\'04\"'
+    height: '3',
     type: ['Grass','Poison']
   },
   {
     name: 'Charmander',
-    height: '2\''
+    height: '2',
     type: 'Fire'
   },
   {
     name: 'Squirtle',
-    height: '1\'08\"'
+    height: '1',
     type: 'Water'
   },
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height > 2.5); {
+    console.log(pokemonList[i]name.height + "Wow, that's big!");
+  }
+  }else if (pokemonList[i].height < 2.5 && > 1.5); {
+    console.log(pokemonList[i]name.height);
+  }else (pokemonList[i].height < 1.5);{
+    console.log(pokemonList[i].name.height);
+  }
