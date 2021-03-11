7bf389574b82db9041f5bd3b3e200308373e6a7a
Travisprice08 <75191962+Travisprice08@users.noreply.github.com>
2021-03-10 19:43:50 -0800

    Updated script.js
    Removed conditional for else statement.

--- js/scripts.js
+++ js/scripts.js
@@ -17,11 +17,12 @@ let pokemonList = [
 ];
 
 for (let i = 0; i < pokemonList.length; i++) {
-  if (pokemonList[i].height > 2.5) {
-    console.log(pokemonList[i].name + ' (Height: ' + pokemonList[i].height + ')'+ " Wow, that's big!")
+    if (pokemonList[i].height > 2.5) {
+      // you were trying to call name at height at the same time. These are seperate properties so you need to call them individually.
+    console.log(pokemonList[i].name + ' (Height: ' + pokemonList[i].height + ')'+ ' -' + " Wow, that's big!")
   }else if (pokemonList[i].height < 2.5 && pokemonList[i].height > 1.5) {
     console.log(pokemonList[i].name + ' (Height: ' + pokemonList[i].height + ')')
-  }else (pokemonList[i].height);{
+  }else{
     console.log(pokemonList[i].name + ' (Height: ' + pokemonList[i].height + ')')
   }
 }
