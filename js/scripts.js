b36fd73f1afde959dde3fce9c9888b5ecaad4ae5
Travisprice08 <75191962+Travisprice08@users.noreply.github.com>
2021-03-15 21:44:57 -0700

    1.5 Task Update
    Created IIFE repository
    
    Used add function to add additional pokemon.
    
    Updated forEach() loop code to access IIFE.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,38 @@
+//Object array
 let pokemonList = [
-  {
-    name: 'Bulbasaur',
-    height: '3',
-    type: ['Grass','Poison']
-  },
-  {
-    name: 'Charmander',
-    height: '2',
-    type: 'Fire'
-  },
-  {
-    name: 'Squirtle',
-    height: '1',
-    type: 'Water'
-  },
-];
+    {
+      name: 'Bulbasaur',
+      height: '3',
+      type: ['Grass','Poison']
+    },
+    {
+      name: 'Charmander',
+      height: '2',
+      type: 'Fire'
+    },
+    {
+      name: 'Squirtle',
+      height: '1',
+      type: 'Water'
+    },
+  ];
+// IIFE repository
+let pokemonRepository = (function () {
+  return {
+    add: function(pokemon) {
+      pokemonList.push(pokemon);
+    },
+    getAll: function() {
+      return pokemonList;
+    }
+  }
+})();
 
+//add function
+pokemonRepository.add({ name: 'Pikachu', height: 1, type: 'Electric'});
+console.log(pokemonRepository.getAll() );
+
+//pokemon function added from IIFE public function to access from outside
 pokemonList.forEach(function(pokemon) {
-  console.log(pokemon.name + ' Height: ' + pokemon.height + ' Type: ' + pokemon.type)
+  document.write(pokemon.name + ' Height: ' + pokemon.height + ' Type: ' + pokemon.type + '<br>')
 });
