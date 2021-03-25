8b68b5f4158342f8b2141b752a5cbbcb8adc2a1b
Travisprice08 <75191962+Travisprice08@users.noreply.github.com>
2021-03-24 19:40:29 -0700

    Task 1.6 Revision
    Return addListItem
    
    Extracted addListItem into separate function.
    
    Restructured layout

--- js/scripts.js
+++ js/scripts.js
@@ -18,37 +18,49 @@ let pokemonList = [
   ];
 // IIFE repository
 let pokemonRepository = (function () {
-  return {
-    add: function(pokemon) {
-      pokemonList.push(pokemon);
-    },
-    getAll: function() {
-      return pokemonList;
-    },
-    addListItem: function(pokemon){
 
+  function add(pokemon) {
+    if (typeof (pokemon) ==='object') {
+      pokemonList.push(pokemon)
     }
   }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  function showdetails(pokemon) {
+    console.log(pokemon)
+  }
+
+  function buttonListener (button, poke) {
+    button.addEventListener('click', function () {
+      showdetails(poke);
+    });
+  }
+
+  function addListItem () {
+    let pokemonList = document.querySelector('.pokemon-list');
+    let listPokemon = document.createElement('li');
+    let button = document.createElement('button');
+    button.innerText = pokemon.name;
+    button.classList.add('pokedex-button');
+    listPokemon.appendChild(button);
+    pokemonList.appendChild(listPokemon);
+  }
+
+  return {
+    add: add,
+    getAll: getAll,
+    addListItem: addListItem,
+  };
 })();
 
 //add function
 pokemonRepository.add({ name: 'Pikachu', height: 1, type: 'Electric'});
 console.log(pokemonRepository.getAll() );
 
-function showdetails(pokemon) {
-  console.log(pokemon)
-}
 //pokemon function added from IIFE public function to access from outside
-pokemonRepository.getAll().forEach(function(pokemon)  {
-  let pokemonList = document.querySelector('.pokemon-list');
-  let listPokemon = document.createElement('li');
-  let button = document.createElement('button');
-  button.innerText = pokemon.name;
-  button.classList.add('pokedex-button');
-  listPokemon.appendChild(button);
-  pokemonList.appendChild(listPokemon);
-  button.addEventListener('click', function () {
-    showdetails(pokemon);
-  });
-
+pokemonRepository.getAll().forEach(function(pokemon){
+  pokemonRepository.addListItem(pokemon);
 });
